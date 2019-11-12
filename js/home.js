dccd667d9d4b8290434c2db85c89675b084e60f5
Lola Marotta <lmarotta@udesa.edu.ar>
2019-11-11 22:58:31 -0300

    borrador js home

--- js/home.js
+++ js/home.js
@@ -1,7 +1,8 @@
 window.addEventListener('load',function() {
 
-var API_KEY = "d8a0184cd857dc924c05b43e5a4f7274"
-var URL_POPULARES = "https://api.themoviedb.org/3/tv/popular?api_key="+API_KEY+"&page=1"
+var API_KEY = "d8a0184cd857dc924c05b43e5a4f7274";
+var URL_POPULARES = "https://api.themoviedb.org/3/tv/popular?api_key="+API_KEY+"&page=1";
+var URL_MEJOR_PUNTUADAS = "https://api.themoviedb.org/3/tv/top_rated?"+API_KEY+"api_key=<<api_key>>&page=1";
 //URL_MEJOR_PUNTUADAS
 //URL_AL_AIRE_HOY
 
@@ -31,6 +32,23 @@ fetch(URL_POPULARES)
     }
 
   });
+
+
+// MEJORES PUNTUADAS
+//fetch(URL_MEJOR_PUNTUADAS)
+//  .then(function(respuesta) {
+//    return respuesta.json();
+//  })
+//  .then(function(datos){
+//    console.log(datos);
+
+//  var arrayPuntuadas = datos.results
+
+//  for (var i = 0; i < arrayPuntuadas.length; i++) {
+//    var ulPuntuadas = document.querySelector(".mayorpuntaje ul")
+
+//    var imgURLDos = ""
+//  }
 
 
 })
