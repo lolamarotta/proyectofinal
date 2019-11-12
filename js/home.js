aa776cd4202446a473f5d1412df410c759185e48
Lola Marotta <lmarotta@udesa.edu.ar>
2019-11-12 12:50:27 -0300

    series top rated vistas en html falta darles estilo

--- js/home.js
+++ js/home.js
@@ -2,7 +2,7 @@ window.addEventListener('load',function() {
 
 var API_KEY = "d8a0184cd857dc924c05b43e5a4f7274";
 var URL_POPULARES = "https://api.themoviedb.org/3/tv/popular?api_key="+API_KEY+"&page=1";
-var URL_MEJOR_PUNTUADAS = "https://api.themoviedb.org/3/tv/top_rated?"+API_KEY+"api_key=<<api_key>>&page=1";
+var URL_MEJOR_PUNTUADAS = "https://api.themoviedb.org/3/tv/top_rated?api_key="+API_KEY+"&page=1";
 //URL_MEJOR_PUNTUADAS
 //URL_AL_AIRE_HOY
 
@@ -35,29 +35,32 @@ fetch(URL_POPULARES)
 
 
 // MEJORES PUNTUADAS
-//fetch(URL_MEJOR_PUNTUADAS)
-//  .then(function(respuesta) {
-//    return respuesta.json();
-//  })
-//  .then(function(datos){
-//    console.log(datos);
+fetch(URL_MEJOR_PUNTUADAS)
+  .then(function(respuesta) {
+    return respuesta.json();
+  })
+  .then(function(datos){
+    console.log(datos);
+  var arrayPuntuadas = datos.results;
 
-//  var arrayPuntuadas = datos.results
+  for (var i = 0; i < arrayPuntuadas.length; i++) {
+    datos.results[i]
+    console.log(datos.results[i].name);
+    console.log(datos.results[i].id);
+    console.log(datos.results[i].poster_path);
 
-//  for (var i = 0; i < arrayPuntuadas.length; i++) {
-//    var ulPuntuadas = document.querySelector(".mayorpuntaje ul")
-
-//    var imgURLDos = "https://image.tmdb.org/tv/popular"+ arrayPuntuadas[i].poster_path;
-
-//    ulPuntuadas.innerHTML +=
-//        <li>
-//          <a href="detalle.html?id=${arrayPuntuadas[i].id}">
-//          <img src="${imgURLDos}" alt="">
-//          </a>
-//        </li>
-//        `;
-//  }
-//  });
+    var ulPuntuadas = document.querySelector(".mayorpuntaje")
+    var elementsHtml = '<li>';
+      elementsHtml += '<div class="uk-panel"'
+      elementsHtml += '<a href="detalle.html?id=${datos.results[i].id}">'
+      elementsHtml += '<img src="https://image.tmdb.org/t/p/original' + datos.results[i].poster_path + '" elements>'
+      elementsHtml += '</div> </li>'
+      ulPuntuadas.innerHTML += elementsHtml
+    }
+  })
+  .catch(function(errors) {
+    console.log(errors);
+  });
 
 
 })
