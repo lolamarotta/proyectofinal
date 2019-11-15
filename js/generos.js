cc743f7dc1c57041b8c451018195760322ff4277
Maca <mamontoya@udesa.edu.ar>
2019-11-15 12:16:15 -0300

    imagenes en genero

--- js/generos.js
+++ js/generos.js
@@ -5,7 +5,7 @@ window.addEventListener('load',function() {
 var API_KEY = "d8a0184cd857dc924c05b43e5a4f7274";
 var URL_GENEROS = 'https://api.themoviedb.org/3/genre/tv/list?api_key=' + API_KEY;
 
-var arrayDeImagenes = ['posteraccion.jpg', 'posteranimacion.jpg', 'postercomedia.jpg', 'postercrimen.jpg', 'posterdocu.jpg', 'posterdrama.jpg', 'posterflia.jpg'];
+var arrayDeImagenes = ['posteraccion.jpg', 'posteranimacion.jpg', 'postercomedia.jpg', 'postercrimen.jpg', 'posterdocu.jpg', 'posterdrama.jpg', 'posterflia.jpg', 'posterkids.jpg','postermisterio.jpg', 'posternews.jpg', 'posterreality.jpg', 'posterciencia.jpg','posteresperanza.jpg', 'postertalk.jpg', 'posterwar.jpg','posterwestern.jpg'];
 
 fetch(URL_GENEROS)
   .then(function(respuesta) {
