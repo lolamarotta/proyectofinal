1cc2e3bfcc59d6dd09c4f4b516bdb13b078b56c6
Maca <mamontoya@udesa.edu.ar>
2019-11-15 14:08:08 -0300

    mini cambios

--- js/resultados-generos.js
+++ js/resultados-generos.js
@@ -8,6 +8,8 @@ var API_KEY = "d8a0184cd857dc924c05b43e5a4f7274";
 var query = new URLSearchParams(location.search);
 
 var idGenero = query.get('idGenero');
+var nombreGenero = query.get('nombreGenero');
+
 console.log(idGenero);
 var URL_GENEROS = 'https://api.themoviedb.org/3/discover/tv?api_key=' + API_KEY + '&sort_by=popularity.desc&page=1&with_genres='+ idGenero;
 
