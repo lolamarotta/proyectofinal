9e0b3037998ad1cc45e9bd670463c347289793b6
Maca <mamontoya@udesa.edu.ar>
2019-11-15 13:16:29 -0300

    cambio resultados

--- js/resultados-generos.js
+++ js/resultados-generos.js
@@ -3,12 +3,12 @@ window.addEventListener('load',function() {
 // API GENEROS
 
 var API_KEY = "d8a0184cd857dc924c05b43e5a4f7274";
-var URL_GENEROS = 'https://api.themoviedb.org/3/discover/tv?api_key=' + API_KEY + '&sort_by=popularity.desc&page=1&with_genres=18';
 
 // 1. Capturo el id que vino en el query string
 var query = new URLSearchParams(location.search);
 
 var idGenero = query.get('idGenero');
+var URL_GENEROS = 'https://api.themoviedb.org/3/discover/tv?api_key=' + API_KEY + '&sort_by=popularity.desc&page=1&with_genres='+ idGenero;
 
 fetch(URL_GENEROS)
   .then(function(respuesta) {
@@ -16,9 +16,9 @@ fetch(URL_GENEROS)
   })
   .then(function(datos){
     var arrayDeResultados = datos.results;
+    console.log(datos);
 
     // Array vacio donde metere las series que coincidan con el ide de genero`
-    var
   })
   .catch(function(errors) {
     console.log(errors);
