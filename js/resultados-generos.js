91c75c57d93d9e99e9a566eb50b1ae330a0a3310
Maca <mamontoya@udesa.edu.ar>
2019-11-15 13:32:32 -0300

    cambio y avance en resultados x genero

--- js/resultados-generos.js
+++ js/resultados-generos.js
@@ -7,7 +7,7 @@ var API_KEY = "d8a0184cd857dc924c05b43e5a4f7274";
 // 1. Capturo el id que vino en el query string
 var query = new URLSearchParams(location.search);
 
-var idGenero = query.get('idGenero');
+var idGenero = query.get('idGeneros');
 var URL_GENEROS = 'https://api.themoviedb.org/3/discover/tv?api_key=' + API_KEY + '&sort_by=popularity.desc&page=1&with_genres='+ idGenero;
 
 fetch(URL_GENEROS)
@@ -18,11 +18,24 @@ fetch(URL_GENEROS)
     var arrayDeResultados = datos.results;
     console.log(datos);
 
+
+for (var i = 0; i < arrayDeResultados.length; i++) {
+  arrayDeResultados[i]
+
+  var elementsHtml = '<li>';
+    elementsHtml += '<div class="uk-panel"'
+    elementsHtml += '<a href="detalle.html?id=${datos.results[i].id}">'
+    elementsHtml += '<img src="https://image.tmdb.org/t/p/original' + datos.results[i].poster_path + '" elements>'
+    elementsHtml += '</div> </li>'
+    ulGeneros.innerHTML += elementsHtml
+}
+
     // Array vacio donde metere las series que coincidan con el ide de genero`
   })
   .catch(function(errors) {
     console.log(errors);
   });
+fetch(URL_GENEROS)
 
 
 
