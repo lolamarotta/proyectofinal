78d46a0a52f00d1992f991224ae9c2acc5b4f3c7
Maca <mamontoya@udesa.edu.ar>
2019-11-16 19:24:28 -0300

    detalles

--- js/detalle.js
+++ js/detalle.js
@@ -16,7 +16,14 @@ fetch(URL_DETALLE)
     // cosas para meter en el html el contenido
     var detalleSerie = document.querySelector('.detalle-serie');
 
+    if (data.poster_path) {
+      detalleSerie.innerHTML += '<img src="https://image.tmdb.org/t/p/original' + data.poster_path + '" >'
+    }
+
     detalleSerie.innerHTML += "<h2>" + data.name + "</h2>";
+    detalleSerie.innerHTML += "<h4>Overview:</h4><p>" + data.overview + "</p>";
+    detalleSerie.innerHTML += "<h4>First air date:</h4><p>" + data.first_air_date + "</p>";
+    detalleSerie.innerHTML += "<h4>Original language:</h4><p>" + data.original_language + "</p>";
   })
   .catch(function (error) {
     console.log(error);
--- js/resultados-generos.js
+++ js/resultados-generos.js
@@ -14,33 +14,33 @@ document.querySelector(".nombre-generos").innerHTML=nombreGenero;
 console.log(idGenero);
 var URL_GENEROS = 'https://api.themoviedb.org/3/discover/tv?api_key=' + API_KEY + '&sort_by=popularity.desc&page=1&with_genres='+ idGenero;
 
-fetch(URL_GENEROS)
-  .then(function(respuesta) {
-    return respuesta.json();
-  })
-  .then(function(datos){
-    var arrayDeResultados = datos.results;
-    console.log(datos);
+// fetch(URL_GENEROS)
+//   .then(function(respuesta) {
+//     return respuesta.json();
+//   })
+//   .then(function(datos){
+//     var arrayDeResultados = datos.results;
+//     console.log(datos);
+//
+//       // var ulGeneros= document.querySelector(".generos")
+//       for (var i = 0; i < arrayDeResultados.length; i++) {
+//         arrayDeResultados[i]
+//
+//         var elementsHtml = '<li>';
+//           elementsHtml += '<div class="uk-panel"'
+//           elementsHtml += `<a href="detalle.html?id=${datos.results[i].id}">`
+//           elementsHtml += '<img src="https://image.tmdb.org/t/p/original' + datos.results[i].poster_path + '" elements>'
+//           elementsHtml += '</div> </li>'
+//           ulGeneros.innerHTML += elementsHtml
+//       }
+//
+//     // Array vacio donde metere las series que coincidan con el ide de genero`
+//   })
+//   .catch(function(errors) {
+//     console.log(errors);
+//   });
 
-// var ulGeneros= document.querySelector(".generos")
-for (var i = 0; i < arrayDeResultados.length; i++) {
-  arrayDeResultados[i]
-
-  var elementsHtml = '<li>';
-    elementsHtml += '<div class="uk-panel"'
-    elementsHtml += `<a href="detalle.html?id=${datos.results[i].id}">`
-    elementsHtml += '<img src="https://image.tmdb.org/t/p/original' + datos.results[i].poster_path + '" elements>'
-    elementsHtml += '</div> </li>'
-    ulGeneros.innerHTML += elementsHtml
-}
-
-    // Array vacio donde metere las series que coincidan con el ide de genero`
-  })
-  .catch(function(errors) {
-    console.log(errors);
-  });
-
-  fetch("https://api.themoviedb.org/3/discover/tv?api_key=e31dd59fefbc10e65215ecd077762f57&language=es-ES&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_generes="+idGenero+"")
+  fetch("https://api.themoviedb.org/3/discover/tv?api_key=e31dd59fefbc10e65215ecd077762f57&language=es-ES&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres="+idGenero+"")
     .then(function(respuesta) {
       return respuesta.json();
     })
@@ -50,7 +50,7 @@ for (var i = 0; i < arrayDeResultados.length; i++) {
       for (var i = 0; i < arrayDeResultados.length; i++) {
         var png = arrayDeResultados[i].poster_path;
         var id = arrayDeResultados[i].id
-        document.querySelector(".grid-genero").innerHTML += '<li class="li-item" tabindex="0"><a href="resultados-por-genero.html?idpeli= '+ id +' "><img class="img-li" src="https://image.tmdb.org/t/p/w185'+png+'"></a>'
+        document.querySelector(".grid-genero").innerHTML += '<li class="li-item" tabindex="0"><a href="detalle.html?id='+ id +' "><img class="img-li" src="https://image.tmdb.org/t/p/w185'+png+'"></a>'
 
       }
      })
